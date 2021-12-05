2870d74c56a1817e5eb683fba66de1306f9d0afa
Markus Haunerdinger <markus-haunerdinger@web.de>
2021-12-05 22:32:06 +0100

    FEATURE: Added files and npm module for tiny-slider

---
+++ Resources/Private/JavaScript/slider.js
@@ -0,0 +1,8 @@
+import { tns } from '../../../node_modules/tiny-slider/src/tiny-slider.js'
+
+var slider = tns({
+    container: '.slider-inner',
+    items: 3,
+    autoplay: false,
+    controlsText: ['zurück', 'weiter']
+});
